10fae24fb1018dc0e3e21539c909d168ed4a5780
Wooseok Yang <wooseok.yang@check24.de>
2026-07-02 14:42:18 +0200

    go-chi 백엔드 라이브러리 추가함.

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,8 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
 
 func main () {
-	fmt.Println("hi hello")
-	return
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func (w http.ResponseWriter, r *http.Request) {
+		_, err := w.Write([]byte("Hello World"))
+		if err != nil {
+			fmt.Printf("err: %v\n", err)
+		}
+
+	})
+	if err := http.ListenAndServe(":3000",r); err != nil {
+		fmt.Printf("err: %v\n", err)
+	}
+
 }
